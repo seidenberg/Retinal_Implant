9f475d70eeee2ca15fe429f26f476c18a895cf20
seidenberg <epseid@gmail.com>
2019-04-02 11:35:18 +0200

    Create repository, add python file, insert header

---
+++ Ex2_Visual.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+'''
+Computer Simulations of Sensory Systems
+Exercise 2
+Retinal Implant Simulation
+
+version: 1.0
+authors: Vitaliy Banov, Savina Kim, Ephraim Seidenberg
+emails: vitanov@ruri.waseda.jp, savkim@ethz.ch, sephraim@ethz.ch
+date: March 29 2019
+'''
