63393573476d8ad760a9fbe6515e34417f800b81
seidenberg <epseid@gmail.com>
2019-04-26 19:34:28 +0200

    created Gabor filterbanks with cv2 (doesn't run without failure yet)

--- Ex2_Visual.py
+++ Ex2_Visual.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 Computer Simulations of Sensory Systems
 Exercise 2
 Retinal Implant Simulation
@@ -8,4 +8,95 @@ version: 1.0
 authors: Vitaliy Banov, Savina Kim, Ephraim Seidenberg
 emails: vitanov@ruri.waseda.jp, savkim@ethz.ch, sephraim@ethz.ch
 date: March 29 2019
-'''
+"""
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from tkinter import filedialog
+# from matplotlib.widgets import Slider
+from numpy import exp, sin, cos, pi
+
+
+# show only dialog
+# root = tk.Tk()
+# root.withdraw()
+
+# open a dialog to select the input image and store its path
+image_path = filedialog.askopenfilename()
+# path to test file: 'C:/Users/epsei/Desktop/Studium/nsc/CSS/Exercises/Ex_Visual/Images/All_images/IMG_3545.jpg'
+
+# read in the selected image
+image = plt.imread(image_path)
+# display the image so that a fixation point can be selected TODO: add caption
+# plt.imshow(image)
+# allow exactly one selection and store the rounded coordinates of the selected fixation point
+# fixation_point = np.round(np.array(plt.ginput(1)))
+# # store the larger of the two distances from fixation point to left and right side of the image.
+# x_max = max(image.shape[0]-fixation_point[0][0], fixation_point[0][0])
+# # store the larger of the two distances from fixation point to top and bottom of the image
+# y_max = max(image.shape[1]-fixation_point[0][1], fixation_point[0][1])
+# # compute the largest distance inside the image starting from the fixation point
+# largest_distance = np.sqrt(x_max**2 + y_max**2)
+
+# print(image.shape, x_max, y_max, largest_distance)
+
+# set the number of different orientations used as requested by the exercise instructions
+n_orientations = 6
+# set the width of the kernel (representing a square shaped receptive field) in pixels
+kernel_width = 5
+# define the width of the gaussian envelope (standard deviation)
+std_dev = 0.5
+# set the ellipticity of the gaussian to 1 (circular)
+ellipticity = 1
+# set the phase offset of the wave function (cosine factor in the Gabor function) to 0
+phase_shift = 0
+# set the wavelength to some reasonable value
+wavelength = 1
+
+
+# set the orientations of features to which the filter should be most responsive as requested in the exercise (use radians)
+orientations = np.linspace(0, 150, n_orientations) * pi / 180
+# place the pixels on the side of the kernel on a space from -1 to 1
+kernel_side = np.linspace(-1, 1, kernel_width)
+# make a square grid for the kernel, separate x and y values
+x, y = np.meshgrid(kernel_side, kernel_side)
+# compute all x' values to be entered into the Gabor function
+x_primes = [x * cos(orientation) + y * sin(orientation) for orientation in orientations]
+# compute all x' values to be entered into the Gabor function
+y_primes = [-x * sin(orientation) + y * cos(orientation) for orientation in orientations]
+
+# compute all kernels applying the Gabor function to each value and store them all into a list
+kernels = [np.array(exp(-(x_prime ** 2 + ellipticity ** 2 * y_prime ** 2) / 2 * std_dev ** 2) * cos(2 * pi * x_prime / wavelength + phase_shift), dtype=np.float32) for x_prime, y_prime in zip(x_primes, y_primes)]
+
+# convert the image to grayscale and store it in matrix form
+image_sw = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)/255.
+
+filtered = cv2.filter2D(image_sw, cv2.CV_32F, kernels[0])
+
+# filter the image with every kernel and store the results in a list
+# filtered = [cv2.filter2D(image_sw, cv2.CV_32F, kernel) for kernel in kernels]
+
+
+
+# cv2.imshow(sum(filtered))
+
+# gabor function: exp
+# def remotest_corner(point, shape):
+#     x_ratio=point/shape[0]
+#     y_ratio=point/shape[1]
+#     if point/shape[1]>.5:
+#         if point
+#
+# print(fixation_point, ' selected')
+#
+# image_gray = image[:,:,0]
+#
+# plt.set_cmap('gray')
+# plt.imshow(image_gray)
+# image_gray.shape
+
+# image_adjusted = image.copy()
+# image_adjusted[200:220, 980:1000] = 160
+# image_adjusted[980:1000, 200:220] = 255
+# plt.imshow(image_adjusted)
